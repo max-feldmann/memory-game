5f68755cc61c1b200a7f5c74755905cd44a781ff
Max Feldmann <maxfeldmann.consultant@gmail.com>
2020-02-19 13:44:02 +0100

    Board can be populated with pairs of random Symbols (from A-Z)

--- board.rb
+++ board.rb
@@ -11,10 +11,31 @@ class Board
         @grid = Array.new(@board_size) {Array.new(@board_size)}
     end
 
+    attr_reader :grid
+
+    # -- UTILITY METHODS --
+    
+    def [](pos)
+        row, col = pos
+        @grid[row][col]
+    end
+
+    def []=(pos, value)
+      row, col = pos
+      @grid[row][col] = value
+    end
+    
+
+    def populate!
+        number_of_pairs = ( @board_size * @board_size ) / 2
+        chosen_values = (VALUES[0...number_of_pairs] * 2).shuffle
+
+        @grid.each_with_index do |row, y|
+            row.each_with_index do |col, x|
+                self[[y, x]] = Card.new(chosen_values.pop)
+            end
+        end
+    end
+
+
 end
-
-b = Board.new
-
-p b
-
-b[0][2] = :X
